ecb7f7d770ec7227a06e084b24eb20c38fa94424
wsu <wsu@SDG77222.sandag.org>
2014-12-17 14:49:32 -0800

    (POPSYN-27) Import PopSynIII

--- src/main/java/org/sandag/popsyn/io/ITargetGrowthFactorDao.java
+++
@@ -1,9 +0,0 @@
-package org.sandag.popsyn.io;
-
-import java.util.HashMap;
-import org.sandag.popsyn.Version;
-
-public interface ITargetGrowthFactorDao
-{
-    HashMap<Integer, Double> getGrowthFactor(int zone, Version version);
-}
